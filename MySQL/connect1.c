b65ecd8ef4269510cebe8d8c5d07e06cb22188c8
wangjiefeng <1414059957@qq.com>
2016-10-14 14:05:15 +0800

    commit

--- MySQL/connect1.c
+++ MySQL/connect1.c
@@ -10,11 +10,17 @@ int main(int argc, char *argv[])
 	
 	if(mysql_real_connect(&my_connection, "localhost", "root", "9082160as", "rick", 0, NULL, 0)) {
 		printf("Connection success\n");
-		ret = mysql_query(&my_connection, "INSERT INTO children(fname, age) VALUES('Ann', 3)");
+		/*ret = mysql_query(&my_connection, "INSERT INTO children(fname, age) VALUES('Ann', 3)");
 		if(!ret) {
 			printf("Insert %lu rwos\n", (unsigned long)mysql_affected_rows(&my_connection));
 		} else {
 			fprintf(stderr, "Insert error %d : %s\n", mysql_errno(&my_connection), mysql_error(&my_connection));
+		}*/
+		ret = mysql_query(&my_connection, "UPDATE children SET AGE = 4 WHERE fname = 'Ann'");
+		if(!ret) {
+			printf("Update %lu rows\n", (unsigned long)mysql_affected_rows(&my_connection));
+		} else {
+			fprintf(stderr, "Update error %d: %s\n", mysql_errno(&my_connection), mysql_error(&my_connection));
 		}
 	} else {
 		fprintf(stderr, "Connection failed\n");
