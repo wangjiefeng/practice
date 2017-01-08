505db74c31e800a3a6752e9513a13866c3a14820
= <1414059957@qq.com>
2017-01-08 15:30:48 +0800

    commit

---
+++ UULP/chapt7/hello4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <curses.h>
+
+int main()
+{
+	int i;
+	initscr();
+	clear();
+	for (i = 0; i < LINES; i++) {
+		move(i, i+i);
+		if (i % 2 == 1) {
+			standout();
+		}
+		addstr("Hello, world");
+		if (i % 2 == 1) {
+			standend();
+		}
+		refresh();
+		sleep(1);
+		move(i, i+i);
+		addstr("				");
+	}
+	endwin();
+}
