f1e80c63cd0aac429fdd1b89be7d8cffaf3a68cb
= <1414059957@qq.com>
2017-01-08 15:30:39 +0800

    commit

---
+++ UULP/chapt7/hello2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <curses.h>
+int main()
+{
+	int i;
+	initscr();
+	clear();
+	for (i = 0; i < LINES; i++) {
+		move(i, i+i);
+		if (i%2 == 1) {
+			standout();
+		}
+		addstr("Hello, world");
+		if (i%2 == 1) {
+			standend();
+		}
+	}
+	//move(LINES-1, 0);
+	refresh();
+	getch();
+	endwin();
+}
