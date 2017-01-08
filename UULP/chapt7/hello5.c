b2fc13ee1c441fbcde09c050611e362d23158d8c
= <1414059957@qq.com>
2017-01-08 15:30:52 +0800

    commit

---
+++ UULP/chapt7/hello5.c
@@ -0,0 +1,34 @@
+#include <curses.h>
+#include <stdio.h>
+
+#define LEFTEDGE	10
+#define RIGHTEDGE	30
+#define ROW			10
+
+int main()
+{
+	char *message	= "Hello";
+	char *blank		= "		";
+	int dir = 1;
+	int pos = LEFTEDGE;
+	
+	initscr();
+	clear();
+	
+	while(1) {
+		move(ROW, pos);
+		addstr(message);
+		move(LINES - 1, COLS - 1);
+		refresh();
+		sleep(1);
+		move(ROW, pos);
+		addstr(blank);
+		pos += dir;
+		if (pos >= RIGHTEDGE) {
+			dir = -1;
+		}
+		if (pos <= LEFTEDGE) {
+			dir = 1;
+		}
+	}
+}
