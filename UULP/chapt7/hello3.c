7577f4c31bf0c32968a256de81b35de0e3556a0c
= <1414059957@qq.com>
2017-01-08 15:30:44 +0800

    commit

---
+++ UULP/chapt7/hello3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <curses.h>
+
+int main()
+{
+	int i;
+	initscr();
+	clear();
+	for (i = 0; i<LINES; i++) {
+		move(i , i+i);
+		
+		if (i%2 == 1) {
+			standout();
+		}
+		
+		addstr("Hello, world");
+		
+		if (i%2 == 1) {
+			standend();
+		}
+		sleep(1);
+		refresh();
+		
+	}
+	endwin();
+}
