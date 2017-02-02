e07cdb58c9a9f181b0a191db155d5e94b8163a9f
= <1414059957@qq.com>
2017-02-02 23:28:10 +0800

    commit

--- UULP/chapt5/echostate.c
+++ UULP/chapt5/echostate.c
@@ -1,19 +1,24 @@
-#include <stdio.h>
-#include <termios.h>
+/* echostate.c
+ *   reports current state of echo bit in tty driver for fd 0
+ *   shows how to read attributes from driver and test a bit
+ */
 
-int main()
+#include        <stdio.h>
+#include        <termios.h>
+
+main()
 {
-	struct termios info;
-	int rv;
-	
-	rv = tcgetattr(0, &info);
-	if (rv == -1) {
-		perror("tcgetattr");
-		exit(0);
-	}
-	if (info.c_lflag & ECHO) {
-		printf("echo is on , since its bit is 1\n");
-	} else {
-		printf("echo is OFF, since its bit is 0\n");
-	}
+        struct termios info;
+        int rv;
+
+        rv = tcgetattr( 0, &info );     /* read values from driver      */
+
+        if ( rv == -1 ){
+                perror( "tcgetattr");
+                exit(1);
+        }
+        if ( info.c_lflag & ECHO )
+                printf(" echo is on , since its bit is 1\n");
+        else
+                printf(" echo if OFF, since its bit is 0\n");
 }
--- UULP/chapt5/setecho.c
+++ UULP/chapt5/setecho.c
@@ -1,23 +1,28 @@
-#include <stdio.h>
-#include <termios.h>
+/* setecho.c
+ *   usage:  setecho [y|n]
+ *   shows:  how to read, change, reset tty attributes
+ */
 
-#define oops(s, x) { perror(s); exit(x); }
+#include        <stdio.h>
+#include        <termios.h>
 
-int main(int argc, char *argv[])
+#define  oops(s,x) { perror(s); exit(x); }
+
+main(int ac, char *av[])
 {
-	struct termios info;
-	if (argc == 1) {
+        struct termios info;
+
+        if ( ac == 1 ) 
 		exit(0);
-	}
-	if (tcgetattr(0, &info) == -1) {
-		oops("tcgetattr", 1);
-	}
-	if (argv[1][0] == 'y') {
-		info.c_lflag |= ECHO;
-	} else {
-		info.c_lflag &= ~ECHO;
-	}
-	if (tcsetattr(0, TCSANOW, &info) == -1) {
-		oops("tcsetattr", 2);
-	}
+
+        if ( tcgetattr(0,&info) == -1 )          /* get attribs   */
+		oops("tcgettattr", 1);
+
+        if ( av[1][0] == 'y' )
+                info.c_lflag |= ECHO ;          /* turn on bit    */
+        else
+                info.c_lflag &= ~ECHO ;         /* turn off bit   */
+
+        if ( tcsetattr(0,TCSANOW,&info) == -1 ) /* set attribs    */
+               oops("tcsetattr",2);
 }
