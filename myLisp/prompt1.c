7463499a1e23b6c376c3d6b496e20e45aa62c5e1
= <1414059957@qq.com>
2017-02-18 22:14:45 +0800

    commit

---
+++ myLisp/prompt1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <editline/readline.h>
+#include <editline/history.h>
+
+int main(int argc, char *argv[])
+{
+	puts("Lispy Version 0.0.0.0.1");
+	puts("Press Ctrl+c to Exit\n");
+	while(1) {
+		char *input = readline("lispy>");
+		add_history(input);
+		printf("No you're a %s\n", input);
+		free(input);
+	}
+	return 0;
+}
